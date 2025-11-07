c63b0f35a4854e42c35fc15f59a04c1536fa4ac4
Riley Tan <xuanyun.tan@gmail.com>
2025-11-07 16:11:57 +0800

    Completed form validation

--- script.js
+++ script.js
@@ -1,14 +1,68 @@
 const password = document.querySelector("#password")
 const confirm = document.querySelector("#confirm")
-const submit = document.querySelector("button")
-const invalid = document.querySelector("#invalid")
+const submit = document.querySelector("#create")
+const form = document.querySelector("#form")
+const feedback = document.querySelector("#submission-feedback")
+
+const requiredInputs = document.querySelectorAll(".required")
+
+// listening to user input 
+requiredInputs.forEach(requiredInput => {
+        requiredInput.addEventListener("blur", () => {
+            if (!requiredInput.checkValidity()) {
+                requiredInput.classList.add("error")
+            }
+            else {
+                requiredInput.classList.remove("error")
+            }
+        })
+    } )
+
 
 submit.addEventListener("click", event => {
     event.preventDefault()
-    if (password!=confirm) {
-        
+    feedback.textContent = ""
+    // passwords don't match, no submission
+    if (!checkPassword()) {
+        feedback.textContent = "Invalid fields"
+        return
     }
+    // required inputs are not valid
+    else if (!inputsValid()) {
+        feedback.textContent = "Invalid fields"
+        return 
+    }
+    // all are ok
+    else {
+        feedback.textContent = "Thank you for your submission"
+    }
+
 })
+
+const checkPassword = () => {
+    if (password.value!=confirm.value) {
+        feedback.textContent = "Passwords do not match. "
+        password.classList.add("error")
+        confirm.classList.add("error")
+        return false
+    }
+    password.classList.remove("error")
+    confirm.classList.remove("error")
+    return true
+    
+    
+}
+
+// returns True if all user input is valid
+const inputsValid = () => {
+    for (const input of requiredInputs) {
+        if (!input.checkValidity()) {
+            input.classList.add("error")
+            return false
+        }
+    }
+    return true
+}
 
 const root = document.documentElement
 const moon = document.querySelector(".theme-toggle")
@@ -16,3 +70,4 @@ const moon = document.querySelector(".theme-toggle")
 moon.addEventListener("click", () => {
     root.classList.toggle("dark")
 })
+
